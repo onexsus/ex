ad69b064bebaf6631a3394bdd1e544c3de111d8b
onexsus <boriskho@gmail.com>
2023-12-11 00:14:55 +0200

    About Done +style

---
+++ react-template-proj/pages/About.jsx
@@ -0,0 +1,17 @@
+export function About() {
+  return (
+    <section className="about flex flex-column align-center ">
+      <div className="about-continer">
+      <h1>About</h1>
+        <img src="./img/store-book-about.jpg"></img>
+      <p>
+        At Bookstore, we're passionate about books. Since 2020, our
+        shelves have held a curated collection catering to diverse tastes. We're
+        more than a store; we're a community fostering a love for reading.
+        Explore our varied selection, attend events, and engage in lively
+        discussions. Join us in celebrating the joy of reading!
+      </p>
+      </div>
+    </section>
+  );
+}
